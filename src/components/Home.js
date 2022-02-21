dfb573343dd30617d6c70283136c920efc0ed038
fanita90 <parroquini_90@hotmail.com>
2022-02-21 13:49:09 -0600

    agregando primeros estilos responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
-import"./styles/home.scss"
+import "./styles/home.scss";
+import logo from "../assets/logo.png";
 
 const Home = () => {
   return (
-    <div>
-      <h1>soy el home</h1>
-    </div>
+    <>
+      <div className="containerHome">
+        
+        <img className="logo" alt="logoHome" src={logo} />
+        <h1>Selecciona tu filtro</h1>
+        <div className="btns-home">
+          <botton className="btn-filter">ESTUDIANTES</botton>
+          <botton className="btn-filter">STAFF</botton>
+        </div>
+      </div>
+    </>
   );
 };
 
