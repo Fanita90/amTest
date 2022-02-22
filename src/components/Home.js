457583743a9a8e7c0df7dd2e49ba338e8c03f2a0
fanita90 <parroquini_90@hotmail.com>
2022-02-21 20:56:38 -0600

    estilos de botones principales terminados

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./styles/home.scss";
 import logo1 from "../assets/logo1.png";
+import fav from "../assets/fav.png";
+import add from "../assets/add.png";
+
 
 const Home = () => {
+  
   return (
     <>
       <div className="containerHome">
-      <botton className="btn-sticky">favoritos</botton>
-      <botton className="btn-stycky">STAFF</botton>
+      <div className="btns-fav-add">
+        <botton className="btn-sticky">Favoritos<img className="icon-btn" alt="favorite" src={fav} /></botton>
+        <botton className="btn-sticky">Agregar<img className="icon-btn" alt="add" src={add} /></botton>
+        </div>
+      
         <img className="logo" alt="logoHome" src={logo1} />
         <h1 className="indication">SELECCIONA TU FILTRO</h1>
         <div className="btns-home">
