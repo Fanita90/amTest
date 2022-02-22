f21e7e691916902af958c092418f9877a35f1abd
fanita90 <parroquini_90@hotmail.com>
2022-02-21 21:58:58 -0600

    modificaciones de estilos de css y formato a json

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,35 @@
 import React from "react";
-import "./styles/home.scss";
+import "../styles/home.scss";
 import logo1 from "../assets/logo1.png";
 import fav from "../assets/fav.png";
 import add from "../assets/add.png";
 
-
 const Home = () => {
-  
   return (
     <>
       <div className="containerHome">
-      <div className="btns-fav-add">
-        <botton className="btn-sticky">Favoritos<img className="icon-btn" alt="favorite" src={fav} /></botton>
-        <botton className="btn-sticky">Agregar<img className="icon-btn" alt="add" src={add} /></botton>
-        </div>
-      
-        <img className="logo" alt="logoHome" src={logo1} />
-        <h1 className="indication">SELECCIONA TU FILTRO</h1>
+        <section className="btns-fav-add">
+          <header>
+            <botton className="btn-sticky">
+              Favoritos
+              <img className="icon-btn" alt="favorite" src={fav} />
+            </botton>
+            <botton className="btn-sticky">
+              Agregar
+              <img className="icon-btn" alt="add" src={add} />
+            </botton>
+          </header>
+        </section>
+        <section>
+          <img className="logo" alt="logoHome" src={logo1} />
+          <h1 className="indication">SELECCIONA TU FILTRO</h1>
+        </section>
         <div className="btns-home">
           <botton className="btn-filter">ESTUDIANTES</botton>
           <botton className="btn-filter">STAFF</botton>
         </div>
-        <div className="container-card">
-        
-        </div>
+
+        <div className="container-card"></div>
       </div>
     </>
   );
