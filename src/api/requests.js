24d5bfecb18c7832ab34fa6e9fc689c916a311a6
fanita90 <parroquini_90@hotmail.com>
2022-02-23 20:03:21 -0600

    se cdesplego la data en heroku, se cambió la url del get

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,17 +1,24 @@
 import axios from "axios";
 
-
-
 export const fetchStaff = () => {
   return axios({
-    url: "http://localhost:8000/staff",
+    //url: "http://localhost:8000/staff",
+    url: "https://prueba-api-harry.herokuapp.com/staff",
     method: "get",
   });
 };
 
 export const fetchStudents = () => {
-    return axios({
-      url: "http://localhost:8000/students",
-      method: "get",
-    });
-  };
+  return axios({
+    //url: "http://localhost:8000/students",
+    url: "https://prueba-api-harry.herokuapp.com/students",
+    method: "get",
+  });
+};
+
+export const addStaff = () => {
+  return axios({
+    url: "http://localhost:8000/staff",
+    method: "post",
+  });
+};
